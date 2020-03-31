afc0f9591f602b3579fa5de920b978747f77ce51
Josh Long <josh@joshlong.com>
2020-03-30 17:05:17 -0700

    got to get back on the right PATH!

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+source "$(cd $(dirname $0) && pwd)/env.sh"
+
 APP_NAME=twitter-pinboard-cleanup-job
 JOB_NAME=${APP_NAME}
 SCHEDULER_SERVICE_NAME=scheduler-joshlong
 POSTGRESQL_SERVICE_NAME=twitter-organizer-db
-
-#export PATH=$HOME/bin:$PATH
 
 cf d -f ${APP_NAME}
 cf push -b java_buildpack -u none --no-route --no-start -p target/${APP_NAME}.jar ${APP_NAME}
---
+++ deploy/env.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/bin
+echo "the PATH is... $PATH"
+export PATH=$PATH:$HOME/bin
